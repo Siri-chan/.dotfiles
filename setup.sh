a8dee52827976d4cd937dc4926d9a95a423aef1f
Siri <xd42069xd69420@gmail.com>
2024-01-24 12:40:12 +1100

    Added hyprland and hyprpaper configs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ln -s ~/.dotfiles/nvim/ ~/.config/
+ln -s ~/.dotfiles/hypr/ ~/.config/
 ln -s ~/.dotfiles/alacritty/ ~/.config/
 ln -s ~/.dotfiles/neofetch/ ~/.config/
 ln -s ~/.dotfiles/bashrc/.bashrc ~
