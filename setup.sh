969e41667445f1b9161c152a2e2757eeebb7da93
Siri <xd42069xd69420@gmail.com>
2024-01-24 12:49:35 +1100

    Added waybar configs

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 ln -s ~/.dotfiles/nvim/ ~/.config/
 ln -s ~/.dotfiles/hypr/ ~/.config/
 ln -s ~/.dotfiles/alacritty/ ~/.config/
+ln -s ~/.dotfiles/waybar/ ~/.config/
 ln -s ~/.dotfiles/neofetch/ ~/.config/
 ln -s ~/.dotfiles/bashrc/.bashrc ~
